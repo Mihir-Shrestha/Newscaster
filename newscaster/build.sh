65c8367149415130b206035c06327283afeb40a8
Mihir Shrestha <mihirshr13@gmail.com>
2026-02-15 15:50:04 -0700

    File structure changes and api key removed/changed

--- newscaster/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Build all images with no cache
-docker build --no-cache -t newscaster-api:latest api/
-docker build --no-cache -t newscaster-fetcher:latest fetcher/
-docker build --no-cache -t newscaster-summarizer:latest summarizer/
-docker build --no-cache -t newscaster-tts:latest tts/
